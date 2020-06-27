837252a1d2c9f2d96ca186518d8104fb3b9dad31
chtian <chtian@anfengde.com>
2020-06-27 23:11:01 +0800

    update docment

--- swtc-oracle/tools/createWallet.js
+++
@@ -1,9 +0,0 @@
-var jlib = require('../node_modules/@swtc/lib/cjs');
-var Wallet = jlib.Wallet;
-//方式一
-var w1 = Wallet.generate();
-
-console.log(w1);
-//方式二
-//var w2 = Wallet.fromSecret('ssiUDhUpUZ5JDPWZ9Twt27Ckq6k4C');
-//console.log(w2);
